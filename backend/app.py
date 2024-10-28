97206150e80ce09677e3ed324f0c60a6efc43160
Vaniah Christian Mukisa <mukisa.vaniah@students.mak.ac.ug>
2024-10-28 11:46:10 +0300

    changes

--- backend/app.py
+++ backend/app.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
 # Load the trained model and scaler
-model = joblib.load('C:\\Users\\Karim\\Desktop\\MUKISA_VANIAH_CHRISTIAN\\model\\heart_disease_model.joblib')
-scaler = joblib.load('C:\\Users\\Karim\\Desktop\\MUKISA_VANIAH_CHRISTIAN\\model\\scaler.joblib')
+model = joblib.load('C:\\Users\\Karim\\Desktop\\MUKISA_VANIAH_CHRISTIAN\\backend\\model\\heart_disease_model.joblib')
+scaler = joblib.load('C:\\Users\\Karim\\Desktop\\MUKISA_VANIAH_CHRISTIAN\\backend\\model\\scaler.joblib')
 
 @app.route('/predict', methods=['POST'])
 def predict():
